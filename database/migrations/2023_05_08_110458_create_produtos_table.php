a8235652d468f50b505820d813162d17b05a5688
Guilherme Eduardo Bublitz <gui.e.bublitz@gmail.com>
2023-05-08 08:19:25 -0300

    categorias/produtos migration files

---
+++ database/migrations/2023_05_08_110458_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('categoria_id');
+            $table->string('codigo');
+            $table->string('nome');
+            $table->float('preco', 8, 2);
+            $table->float('preco_promocional', 8, 2);
+            $table->boolean('ativo');
+            $table->timestamps();
+
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
